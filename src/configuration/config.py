f2429d9d01765165d4a56c6ac7601fdbdf74d685
Inteleger <kdz2314461602@gmail.com>
2026-07-16 00:23:34 +0800

    finish config.py

---
+++ src/configuration/config.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+#1. 目录路径
+ROOT_DIR = Path(__file__).parent.parent.parent
+RAW_DATA_DIR = ROOT_DIR / 'data'/ 'raw'
+PROCESSED_DATA_DIR = ROOT_DIR / 'data'/ 'processed'
+MODEL_DIR = ROOT_DIR / 'models'
+LOG_DIR = ROOT_DIR / 'logs'
+
+#2. 文件
+RAW_TRAIN_DATA = 'train.txt'
+RAW_VALID_DATA = 'valid.txt'
+RAW_TEST_DATA = 'test.txt'
+BERT_MODEL_NAME = 'google-bert/bert-base-chinese'
+
+#3. 超参数
+BATCH_SIZE = 16
+LEARNING_RATE = 1e-5
+EPOCHS = 10
+SAVE_STEPS = 50
